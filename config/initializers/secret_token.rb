633224708095df6bd7bcdeab39f72d65b6b776f9
Joseph Michaels <jmichaels@gmail.com>
2011-09-28 04:32:29 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MassQuiz::Application.config.secret_token = 'e03fa7d568e4181078e1566f0a6d3292d139df6990240535abebceab7523183be148122a1c4e6aa0fccf0704656241c683278d1f595fdf9b8cb6e452270f9eb9'
